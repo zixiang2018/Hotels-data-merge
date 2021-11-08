b38627850c98fab056053f99a751bb3626dfc171
Zi Xiang <zixiang.tng.2018@sis.smu.edu.sg>
2021-11-08 14:14:37 +0800

    fix container host

--- main.py
+++ main.py
@@ -35,4 +35,4 @@ def retrieve_hotels_by_destinations():
     return jsonify(result),200
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
